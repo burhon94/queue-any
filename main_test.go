45d02afa92396baea03de2ed9d26201ef228694f
BT <BT@mail.ru>
2020-01-24 22:35:40 +0500

    changed tests after fixes

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ func Test_empty_queue(t *testing.T) {
 
 func Test_queue_with_one_element(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
+	l.addLast(1)
 	if l.lenQueue() != 1 {
 		t.Error("queue size must be one, got: ", l.lenQueue())
 	}
@@ -19,11 +19,11 @@ func Test_queue_with_one_element(t *testing.T) {
 
 func Test_queue_with_multiple_elements(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
-	l.addLastQueue(2)
-	l.addLastQueue(3)
-	l.addLastQueue(4)
-	l.addLastQueue(5)
+	l.addLast(1)
+	l.addLast(2)
+	l.addLast(3)
+	l.addLast(4)
+	l.addLast(5)
 	if l.lenQueue() != 5 {
 		t.Error("queue size must be 5, got: ", l.lenQueue())
 	}
@@ -31,7 +31,7 @@ func Test_queue_with_multiple_elements(t *testing.T) {
 
 func Test_add_element_to_empty_queue(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
+	l.addLast(1)
 	if l.lenQueue() != 1 {
 		t.Error("queue size after adding one element must be one, got: ", l.lenQueue())
 	}
@@ -39,8 +39,8 @@ func Test_add_element_to_empty_queue(t *testing.T) {
 
 func Test_add_element_last_in_queue_with_queue_one_element(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
-	l.addLastQueue(2)
+	l.addLast(1)
+	l.addLast(2)
 	if l.lenQueue() != 2 {
 		t.Error("queue size is one after adding one element must be 2, got: ", l.lenQueue())
 	}
@@ -48,12 +48,12 @@ func Test_add_element_last_in_queue_with_queue_one_element(t *testing.T) {
 
 func Test_add_elements_last_to_queue_with_multiple_elements(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
-	l.addLastQueue(2)
-	l.addLastQueue(1)
-	l.addLastQueue(2)
-	l.addLastQueue(1)
-	l.addLastQueue(2)
+	l.addLast(1)
+	l.addLast(2)
+	l.addLast(1)
+	l.addLast(2)
+	l.addLast(1)
+	l.addLast(2)
 	if l.lenQueue() != 6 {
 		t.Error("queue size is 3 after adding 3 elements must be 6, got: ", l.lenQueue())
 	}
@@ -79,11 +79,11 @@ func Test_remove_first_element_from_queue_with_one_element(t *testing.T) {
 
 func Test_remove_first_element_from_queue_with_multiple_element(t *testing.T) {
 	l := queue{}
-	l.addElementToQueue(1)
-	l.addLastQueue(2)
-	l.addLastQueue(3)
-	l.addLastQueue(4)
-	l.addLastQueue(5)
+	l.addLast(1)
+	l.addLast(2)
+	l.addLast(3)
+	l.addLast(4)
+	l.addLast(5)
 	l.removeFirstFromQueue()
 	if l.lenQueue() != 4 {
 		t.Error("queue size is 5 after remove first element queue size must be 4, got: ", l.lenQueue())
